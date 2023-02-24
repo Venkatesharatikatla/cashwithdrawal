a296b91af0f3ff57b5523bc5a63005f789de3838
Venkateshartikatla <venkatesharatikatla8@gmail.com>
2023-02-24 14:19:22 +0000

    solution

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -1,24 +1,50 @@
 import {Component} from 'react'
 
+import DenominationItem from '../DenominationItem'
+
 import './index.css'
 
 class CashWithdrawal extends Component {
+  state = {
+    balance: 2000,
+  }
+
+  updateBalance = value => {
+    this.setState(prevState => ({balance: prevState.balance - value}))
+  }
+
   render() {
+    const {denominationsList} = this.props
+    const {balance} = this.state
+
     return (
       <div className="container">
         <div className="sub-container">
           <div className="user-details-container">
-            <p className="logo">S</p>
+            <div className="logo-details">
+              <p className="logo">S</p>
+            </div>
             <p className="user-name">Sarah Williams</p>
           </div>
           <div className="balance-details-container">
             <p className="balance">Your Balance</p>
             <p className="amount">
-              200
+              {balance}
               <br />
               <span className="rupees">In Rupees</span>
             </p>
           </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM(IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denominationDetails={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     )
